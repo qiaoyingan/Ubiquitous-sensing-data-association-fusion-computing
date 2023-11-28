61e0e8e438270a71c5569db59ffd0a0f84edfea2
ganqy <735990477@qq.com>
2023-11-28 17:50:10 +0800

    -

--- t2.py
+++ t2.py
@@ -13,104 +13,140 @@ rdseed = 10
 random.seed(rdseed)
 np.random.seed(rdseed)
 
-num_people = 2000
+train_folder, test_folder, result_folder = 'train_dataset/', 'test_dataset/', 'result/'
 
-maxDeltaT = 120  # 120秒为最长采集时差
+path_imsi = train_folder + 'CCF2021_run_record_c_Train.csv'
+path_face = train_folder + 'CCF2021_run_record_p_Train.csv'
 
+dict_label = {} # dict_label is used to identify the correct person-code relation
+device_list = [] # device_list is used to record all the positions (Dxx)
+face_list, code_list = [], [] # xxxx_list is used to record the info of different positions
+window_size, stride = 100, 1000 # identify person and code appear in [time, time + window_size] as co-appear
+co_appear_df = None # record person and code appear in the same time block
 
-def getDeltaSeconds(df_Face, df_Imsi):
-    df_FaceCode = pd.merge_asof(df_Face, df_Imsi, on=["TimeStamp"], by=["FaceLabel", "DeviceID"], tolerance=maxDeltaT,
-                                direction="nearest")
+if __name__ == '__main__':
 
-    print(df_FaceCode)
-    print(df_FaceCode.columns)
-    df_FaceCode["TimeDelta"] = df_FaceCode.apply(
-        lambda x: pd.Timedelta(seconds=0) if (pd.isna(x['Time1_x']) or pd.isna(x['Time1_y'])) else abs(
-            x['Time1_x'] - x['Time1_y']), axis=1)  # 无法处理NaN
-    df_FaceCode['TimeDelta'] = df_FaceCode['TimeDelta'].fillna(pd.Timedelta(seconds=0))
+    df_Imsi = pd.read_csv(path_imsi, dtype=str)
+    df_Imsi.columns = ['DeviceID', 'Lon', 'Lat', 'Time', 'Code']
+    df_Imsi['Time1'] = pd.to_datetime(df_Imsi['Time'])
+    df_Imsi['TimeStamp'] = [int(t.timestamp()) for t in df_Imsi['Time1']]
+    df_Face = pd.read_csv(path_face, dtype=str)
+    df_Face.columns = ['DeviceID', 'Lon', 'Lat', 'Time', 'FaceLabel']
+    df_Face['Time1'] = pd.to_datetime(df_Face['Time'])
+    df_Face['TimeStamp'] = [int(t.timestamp()) for t in df_Face['Time1']]
 
-    df_FaceCode['TimeDeltaSeconds'] = df_FaceCode['TimeDelta'].map(lambda x: x.seconds)
+    path_label = train_folder + 'CCF2021_run_label_Train.csv'
+    df_label = pd.read_csv(path_label, dtype=str)
+    for tup in zip(df_label['人员编号'], df_label['特征码']):
+        dict_label[tup[1]] = tup[0]
 
-    df_min = df_FaceCode.groupby(["FaceLabel", "DeviceID"])["TimeDeltaSeconds"].min()
+    df_Face = df_Face.sort_values(by="TimeStamp")
+    df_Imsi = df_Imsi.sort_values(by="TimeStamp")
+    df_Imsi["FaceLabel"] = df_Imsi['Code'].map(lambda x: dict_label.get(x))
 
-    df_describe = df_min.groupby(["DeviceID"]).describe()
-    df_describe['edge1'] = df_describe['mean'] + 3 * df_describe['std']  # 3倍标准差
-    df_describe['edge2'] = df_describe['75%'] + 1.5 * (df_describe['75%'] - df_describe['25%'])  # 箱线图四分位确定
+    device_list = df_Face['DeviceID'].drop_duplicates().values.tolist()
 
-    # print(df_describe)
-    deltaSeconds = df_describe.apply(lambda x: min(x['edge1'], x['edge2'], x['max']), axis=1)
+    for device in device_list:
+        face_list.append(df_Face.loc[df_Face['DeviceID'] == device])
+        code_list.append(df_Imsi.loc[df_Imsi['DeviceID'] == device])
 
-    return deltaSeconds
+    co_appear_df = pd.DataFrame({'Facelabel' : [], 'Code' : []})
 
+    for _ in range(len(device_list)): # Each position
 
-path_imsi = './train_dataset/CCF2021_run_record_c_Train.csv'
-path_face = './train_dataset/CCF2021_run_record_p_Train.csv'
+        start_time = min(face_list[_]['TimeStamp'].iloc[0], code_list[_]['TimeStamp'].iloc[0])
+        end_time = start_time + window_size
+        the_end = max(face_list[_]['TimeStamp'].iloc[-1], code_list[_]['TimeStamp'].iloc[-1])
+        
+        face_len, code_len = len(face_list[_]), len(code_list[_])
+        print(face_len, code_len)
+        print(" = = = ")
+        face_index, code_index = 0, 0
+        start_face, start_code = 0, 0 # record the start index in last window 
 
-df_Imsi = pd.read_csv(path_imsi, dtype=str)
-df_Imsi.columns = ['DeviceID', 'Lon', 'Lat', 'Time', 'Code']
-df_Imsi['Time1'] = pd.to_datetime(df_Imsi['Time'])
-df_Imsi['TimeStamp'] = [int(t.timestamp()) for t in df_Imsi['Time1']]
-# print(df_Imsi['TimeStamp'])
-df_Face = pd.read_csv(path_face, dtype=str)
-df_Face.columns = ['DeviceID', 'Lon', 'Lat', 'Time', 'FaceLabel']
-df_Face['Time1'] = pd.to_datetime(df_Face['Time'])
-df_Face['TimeStamp'] = [int(t.timestamp()) for t in df_Face['Time1']]
-# print(df_Face['TimeStamp'])
+        # There are still dispute:
+            # 1、when a particular person/code occur more than once in current window, n_pc++? n_p++? n_c++?
+            # 2、when current window have no more person, still count code?
+            # 3、when current window have no more code, still count person?
+        # Current approach: 1: +=1, 2: No, 3: No.
 
-path_label = './train_dataset/CCF2021_run_label_Train.csv'
-df_label = pd.read_csv(path_label, dtype=str)
-dict_label = {}
-for tup in zip(df_label['人员编号'], df_label['特征码']):
-    dict_label[tup[1]] = tup[0]
+        # * the algorithm seems to be very slow, especially set window_size & stripe into small values.
 
-# print(len(dict_label))
+        while 1: # slide window [start_time, end_time], nxt window is [start_time + stride, end_time + stride]
+            print(face_index, code_index, start_face, start_code, start_time, end_time)
+            faces, codes = [], []
+            face_index, code_index = start_face, start_code # prevent to start from 0 for saving time
 
-df_Face = df_Face.sort_values(by="TimeStamp")
-df_Imsi = df_Imsi.sort_values(by="TimeStamp")
-df_Imsi["FaceLabel"] = df_Imsi['Code'].map(lambda x: dict_label.get(x))
-# print(len(df_Face))
-# print(len(df_Imsi))
-df_Face1 = df_Face[['DeviceID', 'TimeStamp', 'FaceLabel']]
-df_Imsi1 = df_Imsi[['DeviceID', 'TimeStamp', 'Code']]
-# deltaSeconds = getDeltaSeconds(df_Face1, df_Imsi1)
+            while 1: # find the persons and codes in current window and add them into faces[], codes[]
+                # print(1)
+                if face_index < face_len:
+                    if face_list[_]['TimeStamp'].iloc[face_index] < start_time:
+                        face_index += 1
+                        start_face = face_index
+                    else:
+                        if face_list[_]['TimeStamp'].iloc[face_index] < end_time:
+                            faces.append(face_list[_]['FaceLabel'].iloc[face_index])
+                            print("faces append {}".format(face_index))
+                            face_index += 1
+                if code_index < code_len:
+                    if code_list[_]['TimeStamp'].iloc[code_index] < start_time:
+                        code_index += 1
+                        start_code = code_index
+                    else:
+                        if code_list[_]['TimeStamp'].iloc[code_index] < end_time:
+                            codes.append(code_list[_]['Code'].iloc[code_index])
+                            print("codes append {}".format(code_index))
+                            code_index += 1
+                if face_index >= face_len or code_index >= code_len:
+                    break
+                if face_list[_]['TimeStamp'].iloc[face_index] >= end_time \
+                    and code_list[_]['TimeStamp'].iloc[code_index] >= end_time:
+                    break
+                
+            for f in faces:
+                for c in codes:
+                    co_appear_df = co_appear_df.append({'Facelabel': f, 'Code': c}, ignore_index=True)
+            # update the window 
+            start_time += stride
+            end_time += stride
+            if start_time > the_end:
+                break
+    print(co_appear_df)
 
-# print(deltaSeconds)
+    # df_Face1 = df_Face.loc[df_Face['DeviceID'] == 'D02']
+    # df_Imsi1 = df_Imsi[['DeviceID', 'TimeStamp', 'Code']]
+    # print(face_list[0], code_list[0])
+    # # 这个做法是对facelabel中的每个人脸选取一个时空最近的硬件码标记为同时出现，
+    # # 后续可以改进的是在某个时间段内出现的所有硬件码都标记为同时出现
+    # df = pd.merge_asof(df_Face1, df_Imsi1, on=["TimeStamp"], by=["DeviceID"], tolerance=maxDeltaT,
+    #                 direction="nearest")
+    # # print(df)
+    # df = df[['FaceLabel', 'Code']]
+    # print(df)
 
+    # # 创建一个矩阵，按照FaceLabel和code进行分组，统计每个组合出现的次数
+    matrix = pd.pivot_table(df, index="FaceLabel", columns="Code", aggfunc="size", fill_value=0)
+    # 打印矩阵
+    print(matrix)
+    print(len(matrix))
+    print(matrix.columns)
+    print(matrix.loc['P0000'].idxmax())  # 根据行索引选择X行的最大值的列名称
+    print(matrix.iloc[0].idxmax())  # 根据行位置选择第一行的最大值的列名称，注意Python的索引从0开始
 
-# 合并两个文件，保留相同的DeviceID和Time
-# df = pd.merge(df_Face1, df_Imsi1, on=["DeviceID", "TimeStamp"], how="inner")
+    matrix['Row_sum'] = matrix.apply(lambda x: x.sum(), axis=1)  # 按行求和，添加为新列
+    matrix.loc['Col_sum'] = matrix.apply(lambda x: x.sum())  # 按列求和，添加为新行
+    print(matrix)
+    ma = matrix
+    # print(matrix.loc['P1995', 'C012FmvR'])
+    # for index, row in matrix.iteritems():
 
-# 这个做法是对facelabel中的每个人脸选取一个时空最近的硬件码标记为同时出现，
-# 后续可以改进的是在某个时间段内出现的所有硬件码都标记为同时出现
-df = pd.merge_asof(df_Face1, df_Imsi1, on=["TimeStamp"], by=["DeviceID"], tolerance=maxDeltaT,
-                   direction="nearest")
-# print(df)
-df = df[['FaceLabel', 'Code']]
-print(df)
+    print(ma.iloc[-1, -1])
+    for in1, (face, row) in enumerate(matrix.iterrows()):
+        for in2, n_pc in enumerate(row):
+            ma.iloc[in1, in2] = np.log(1 + n_pc) * np.log(1 + n_pc/(ma.iloc[-1, in2]-n_pc+1))*np.log(1 + n_pc/(ma.iloc[in1, -1]-n_pc+2))
+            # print(ma.iloc[in1, in2])
+    print(ma)
 
-# 创建一个矩阵，按照FaceLabel和code进行分组，统计每个组合出现的次数
-matrix = pd.pivot_table(df, index="FaceLabel", columns="Code", aggfunc="size", fill_value=0)
-# 打印矩阵
-print(matrix)
-print(len(matrix))
-print(matrix.columns)
-print(matrix.loc['P0000'].idxmax())  # 根据行索引选择X行的最大值的列名称
-print(matrix.iloc[0].idxmax())  # 根据行位置选择第一行的最大值的列名称，注意Python的索引从0开始
-
-matrix['Row_sum'] = matrix.apply(lambda x: x.sum(), axis=1)  # 按行求和，添加为新列
-matrix.loc['Col_sum'] = matrix.apply(lambda x: x.sum())  # 按列求和，添加为新行
-print(matrix)
-ma = matrix
-# print(matrix.loc['P1995', 'C012FmvR'])
-# for index, row in matrix.iteritems():
-
-print(ma.iloc[-1, -1])
-for in1, (face, row) in enumerate(matrix.iterrows()):
-    for in2, n_pc in enumerate(row):
-        ma.iloc[in1, in2] = np.log(1 + n_pc) * np.log(1 + n_pc/(ma.iloc[-1, in2]-n_pc+1))*np.log(1 + n_pc/(ma.iloc[in1, -1]-n_pc+2))
-        # print(ma.iloc[in1, in2])
-print(ma)
-
-# 计算公式 
-# np.log(1+ n_pc)*np.log(1+ n_pc/(n_c-n_pc+1))*np.log(1+ n_pc/(n_p-n_pc+2))
+    # 计算公式 
+    # np.log(1+ n_pc)*np.log(1+ n_pc/(n_c-n_pc+1))*np.log(1+ n_pc/(n_p-n_pc+2))
 
