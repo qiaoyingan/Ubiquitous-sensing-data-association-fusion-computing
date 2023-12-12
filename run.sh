cd193ea17c1287160206fe98fab98792faa6dc1d
ganqy <735990477@qq.com>
2023-12-12 18:11:09 +0800

    bash

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 for ((i = 30; i <= 200; i += 10))
 do 
     for ((j = 5; j <= i; j += 5)) 
-    # do python main_classifier.py $i $j
-    do echo "hello" > res.log
+    do python main_classifier.py $i $j > res.log
     done
 done
 # python main_classifier.py 100 20
