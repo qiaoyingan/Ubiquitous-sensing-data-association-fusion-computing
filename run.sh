2c6ac6d4176c0c0d8e93132bdc7fa8abdc3c920b
ganqy <735990477@qq.com>
2023-12-12 18:19:40 +0800

    bash fix bug

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+> res.log
 for ((i = 30; i <= 200; i += 10))
 do 
     for ((j = 5; j <= i; j += 5)) 
-    do python main_classifier.py $i $j > res.log
+    do python main_classifier.py $i $j
     done
 done
 # python main_classifier.py 100 20
